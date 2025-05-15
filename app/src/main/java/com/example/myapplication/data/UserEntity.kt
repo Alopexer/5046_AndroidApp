64371ce80d8e891bc25911e8871bc4ce56cce458
Alopexer <Alopexer@163.com>
2025-05-15 21:48:44 +1000

    modify some error in login

--- app/src/main/java/com/example/myapplication/data/UserEntity.kt
+++ app/src/main/java/com/example/myapplication/data/UserEntity.kt
@@ -1,9 +1,13 @@
 package com.example.myapplication.data
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "users")
+@Entity(
+    tableName = "users",
+    indices = [Index(value = ["email"], unique = true), Index(value = ["username"], unique = true)]
+)
 data class UserEntity(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val username: String,
